1b32a97ebff4e25b2a9f1083260ba5d730930e1c
Dmitrii Selin <120986503+Dimitrius199@users.noreply.github.com>
2022-12-27 01:09:43 +0500

    initial commit

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        int amount = 2000;
+        int rouble = 20;
+        int miles = amount / rouble;
+        System.out.println("Вы получили " + miles + " miles");
+    }
+}
